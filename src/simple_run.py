716d13330bec12eac3f562f199b0f5c1d1d94dea
hbery <a.twardosz98@gmail.com>
2023-10-31 18:36:27 +0100

    feat: create simple runner script

---
+++ src/simple_run.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# vim: ft=python : ts=4 : sts=4 : sw=4 : et :
+"""
+Runner script.
+"""
+
+
+import sys
+
+from mininet.net import Mininet
+from mininet.node import RemoteController
+from mininet.topo import Topo
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+
+from ..topos.abilene_topo import AbileneTopo
+from ..topos.abilene_mod_topo import AbileneModTopo
+
+
+def simple_run(topo: Topo):
+     "Basic runner script."
+
+     info('*** Creating topology\n')
+     net = Mininet(topo=topo,
+                   controller=lambda name: RemoteController(name,
+                                                            ip='127.0.0.1',
+                                                            port=6653),
+                   )
+
+     info('*** Starting network\n')
+     net.start()
+
+     info('*** Running CLI\n')
+     CLI(net)
+
+     info('*** Stopping network')
+     net.stop()
+
+
+if __name__ == "__main__":
+    setLogLevel('info')
+
+    if len(sys.argv) == 1 or sys.argv[1] == 'base':
+        simple_run(AbileneModTopo)
+    elif sys.argv[1] == 'abilene':
+        simple_run(AbileneTopo)
+    else:
+        print("ERROR: No such topology option.")
+        sys.exit(1)

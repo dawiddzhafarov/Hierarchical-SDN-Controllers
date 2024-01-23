e6cc4df0c643a6215078aba1b638add9c5023ecd
Dawid <56120592+dawiddzhafarov@users.noreply.github.com>
2024-01-23 17:30:45 +0100

    Added traffic generation improvements (#6)

--- scripts/generate_traffic.sh
+++ scripts/generate_traffic.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-./generate_flows.sh -addresses "10.0.0.2 10.0.0.3 10.0.0.6" -name "10.0.0.9" -number 100 -port 8000 # sea -> mci, hou, den
-./generate_flows.sh -addresses "10.0.0.2 10.0.0.3 10.0.0.6" -name "10.0.0.10" -number 100 -port 10000 # sjc -> mci, hou, den
-./generate_flows.sh -addresses "10.0.0.2 10.0.0.3 10.0.0.6" -name "10.0.0.5" -number 100 -port 12000 # lax -> hou, mci, den
+# "Magic" number of flows that can be sent per one line is 400. 
+# It means, that if you send traffic from one to three hosts, 
+# each host can generate 133 flows at most. If you send traffic
+# from one to two hosts, each host might send 200 flows at most.
+
+# Case 1: 3 hosts, each sends traffic to 3 more hosts -> 399 flows per host generated, 1197 flows in total
+./generate_flows.sh -addresses "10.0.0.2 10.0.0.3 10.0.0.6" -name "10.0.0.9" -number 133 -port 8000 # sea -> mci, hou, den
+./generate_flows.sh -addresses "10.0.0.2 10.0.0.3 10.0.0.6" -name "10.0.0.10" -number 133 -port 10000 # sjc -> mci, hou, den
+./generate_flows.sh -addresses "10.0.0.2 10.0.0.3 10.0.0.6" -name "10.0.0.5" -number 133 -port 12000 # lax -> hou, mci, den
+
+# Case 2: 1 host generates 400*9=3600 flows to a single host. One more line causes receiver to error out.
+./generate_flows.sh -addresses "10.0.0.3" -name "10.0.0.9-single-1" -number 400 -port 14000
+./generate_flows.sh -addresses "10.0.0.3" -name "10.0.0.9-single-2" -number 400 -port 14500
+./generate_flows.sh -addresses "10.0.0.3" -name "10.0.0.9-single-3" -number 400 -port 15000
+./generate_flows.sh -addresses "10.0.0.3" -name "10.0.0.9-single-4" -number 400 -port 15500
+./generate_flows.sh -addresses "10.0.0.3" -name "10.0.0.9-single-5" -number 400 -port 16000
+./generate_flows.sh -addresses "10.0.0.3" -name "10.0.0.9-single-6" -number 400 -port 16500
+./generate_flows.sh -addresses "10.0.0.3" -name "10.0.0.9-single-7" -number 400 -port 17000
+./generate_flows.sh -addresses "10.0.0.3" -name "10.0.0.9-single-8" -number 400 -port 17500
+./generate_flows.sh -addresses "10.0.0.3" -name "10.0.0.9-single-9" -number 400 -port 18000

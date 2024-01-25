ee6a34b1a46fa9b8119e379bb8d0d92618eee07b
hbery <a.twardosz98@gmail.com>
2024-01-25 09:34:25 +0000

    tr

--- scripts/generate_traffic.sh
+++ scripts/generate_traffic.sh
@@ -23,14 +23,14 @@
 
 # Case 2: 1 host (SJC) generates 3600 flows to a single host (MCI).
 ./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-1" -number 400 -port 14000
-./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-2" -number 400 -port 14500
-./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-3" -number 400 -port 15000
-./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-4" -number 400 -port 15500
-./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-5" -number 400 -port 16000
-./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-6" -number 400 -port 16500
-./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-7" -number 400 -port 17000
-./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-8" -number 400 -port 18500
-./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-9" -number 400 -port 18000
+#./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-2" -number 400 -port 14500
+#./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-3" -number 400 -port 15000
+#./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-4" -number 400 -port 15500
+#./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-5" -number 400 -port 16000
+#./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-6" -number 400 -port 16500
+#./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-7" -number 400 -port 17000
+#./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-8" -number 400 -port 18500
+#./generate_flows.sh -addresses "10.0.0.6" -name "10.0.0.10-single-9" -number 400 -port 18000
 
 
 ./generate_flows.sh -addresses "10.0.0.2" -name "10.0.0.5-single-1" -number 400 -port 14000
